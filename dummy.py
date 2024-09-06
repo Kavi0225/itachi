33240e3a8b56f1612361753e06d37de57819d940
heart less <kavi02k25@gmail.com>
2024-09-06 18:39:18 +0530

    modified

--- dummy.py
+++ dummy.py
@@ -1,2 +1,13 @@
-print("changed")
+a = [ 23,45,6,7,8,100,49]
+
+even = []
+odd = []
+for i in a:
+    if i % 2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
+print(even)
+print(odd)
+
 
