00b81f36e87b117c55fd43aa81b6e38667f0ac47
Kaviyarasan L <163851202+Kavi0225@users.noreply.github.com>
2024-09-19 23:53:20 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -1,66 +1,50 @@
 import streamlit as st
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_community.chat_models import ChatOpenAI
+from langchain.embeddings import CohereEmbeddings
+from langchain.vectorstores.faiss import FAISS
+from langchain.llms.cohere import Cohere
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.chat_models import cohere
 
-st.header("My new RAG")
+st.header("My new RAG  AI")
 
 with st.sidebar:
-    st.title("Upload your pdf")
-    file = st.file_uploader("place your pdf here")
+    st.title("Upload your PDF")
+    file = st.file_uploader("Place your PDF here")
 
-OPEN_AI_KEY = "EeQ1K2fvpfzNdjDlup5aCHkRGoN7krq81iKwzcVg"
+COHERE_API_KEY = "openai key"
+
 if file is not None:
-    Pdf_reader = PdfReader(file)
+    pdf_reader = PdfReader(file)
     text = ""
-    for page in Pdf_reader.pages:
+    for page in pdf_reader.pages:
         text += page.extract_text()
-        # st.write(text)
 
-    text_spliter = RecursiveCharacterTextSplitter(
+    text_splitter = RecursiveCharacterTextSplitter(
         separators=["\n"],
         chunk_size=1000,
         chunk_overlap=100,
         length_function=len
     )
 
-    chunk = text_spliter.split_text(text)
+    chunks = text_splitter.split_text(text)
 
-    embeddings = OpenAIEmbeddings(open_ai_key=OPEN_AI_KEY)
+    embeddings = CohereEmbeddings(cohere_api_key=COHERE_API_KEY)
+    vector_storage = FAISS.from_texts(chunks, embeddings)
 
-    vector_storage = FAISS.from_texts(chunk, embeddings)
-
-    user_question = st.file_uploader("type your questions")
+    user_question = st.text_input("Type your question")
 
     if user_question:
         match = vector_storage.similarity_search(user_question)
-        # st.write(match)
 
-        llm = cohere(
-            open_ai_key=OPEN_AI_KEY,
-            temperature=0,
-            max_token=100,
-            model_name="cohere"
+        llm = Cohere(
+            cohere_api_key=COHERE_API_KEY,
+            model="medium",
+            temperature=0.0,
+            max_tokens=100
         )
 
         chain = load_qa_chain(llm, chain_type="stuff")
-        respon = chain.run(input_document=match, question=user_question)
-        st.write(respon)
+        response = chain.run(input_documents=match, question=user_question)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        st.write(response)
